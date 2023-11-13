ae4271489521d4caad734474291756a199a0589a
LauSalmon <salmon.laura@laposte.net>
2023-11-13 10:20:28 +0100

    modification main.3js

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ const s_nom = document.getElementById("name");
 const s_prenom = document.getElementById("firstname");
 const s_mail = document.getElementById("email");
 
-let nom = s_nom.value;
-let prenom = s_prenom.value;
-let mail = s_mail.value;
+
 
 
 
 //Faire un evenement au click sur la sourie
 const array = document.querySelector("#bt");
 array.addEventListener("click", function() {
+    let nom = s_nom.value;
+    let prenom = s_prenom.value;
+    let mail = s_mail.value;
+    
     let tab= [nom,prenom,mail];
     console.log(tab);
 })
