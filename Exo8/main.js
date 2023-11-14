43f75b0d1766cdbdee3c23db096574976eef0197
LauSalmon <salmon.laura@laposte.net>
2023-11-14 14:42:48 +0100

    modif Exo8/main.js

--- Exo8/main.js
+++ Exo8/main.js
@@ -9,15 +9,19 @@ for (let i=0; i<how; i++) {
 
 let positive = 0;
 let negative = 0;
+let big = 0;
 
 for (let j=0; j<how; j++) {
-    if (array[j] < 0) {
-        negative ++;
-    } else {
+    if (array[j] > 0) {
         positive ++;
+        if (array[j]> big){
+            big = array[j];
+        }
+    } else {
+        negative ++;
     }
 }
 
-
 console.log("Nombre de valeur positives :" + positive);
 console.log("Nombre de valeur negatives :" + negative);
+console.log(big + " est la valeur la plus grande");
