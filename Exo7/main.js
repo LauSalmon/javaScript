011cf48c8b1fc3dc4ec0ff9384d9c60105537535
LauSalmon <salmon.laura@laposte.net>
2023-11-14 14:16:47 +0100

    modif Exo7/main.js

--- Exo7/main.js
+++ Exo7/main.js
@@ -1,16 +1,27 @@
 let users =[];
-let ages =[];
-// boucle avec les  demande d'info
-for (let i=0; i<3; i++){
-    let user = {
-    nom : prompt("Saisir votre nom"),
-    prenom : prompt("Saisir votre prénom"),
-    age : parseInt(prompt("Saisir votre age")),
+
+// boucle avec les  5 utilisateurs
+for (let i=0; i<5; i++){
+    //création du  tableau associatif
+    const user = {
+    nom : prompt("Saisir votre nom" + (i+1)),
+    prenom : prompt("Saisir votre prénom"+ (i+1)),
+    age : parseInt(prompt("Saisir votre age"+ (i+1))),
     }
 users.push(user);
-ages.push(user.age);
 }
 
-let jeune = Math.min(...ages);
+//stocker l'age minimum
+let jeune = users[0].age; // age est dans un tableau associatif qui est lui-meme dans un tableau indexé
 
-console.log(jeune);
+//stocker l'indice
+let indice =0;
+// boucle pour parcourir le tableau users
+for (let i=0; i<users.length; i++) {
+    if (users[i].age < jeune){
+        jeune = user[i].age;
+        indice = i;
+    }
+}
+
+console.log(`L'utilisateur :  ${users[indice].nom} ${users[indice].prenom} est le plus jeune`);
