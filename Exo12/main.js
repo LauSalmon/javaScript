7e804fb262c78fa9dddc325c16dffb08826314bd
LauSalmon <salmon.laura@laposte.net>
2023-11-15 11:29:01 +0100

    modif Exo12/main.js

--- Exo12/main.js
+++ Exo12/main.js
@@ -1,5 +1,7 @@
 let number = parseInt(prompt("Vous voudiez connaitre quelle table de multiplication ?"));
 
+console.log("Table de " + number + " :");
+
 for (let i=0; i<11; i++) {
     console.log(number + " x " + i + " = " + (number*i));
 }
