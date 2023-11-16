798a2e003a00a2fd510473f4e2fccc847aa39cef
LauSalmon <salmon.laura@laposte.net>
2023-11-16 10:21:23 +0100

    modif Class3/main.js

--- Class3/main.js
+++ Class3/main.js
@@ -5,7 +5,7 @@ class IMC {
         this.taille = taille;
     }
     calcul(){
-        return this.poid/((this.taille*this.taille));
+        return this.poid/(this.taille**2);
     }
     display() {
         console.log(this.nom + " mesure " + this.taille + "m et pèse " + this.poid + "kg donc son IMC est de " + this.calcul().toFixed(2));
@@ -14,4 +14,23 @@ class IMC {
 
 let imc = new IMC ("Jade", 60, 1.65);
 
+//Tableau
+let list =[];
+list.push(new IMC("Jade", 60, 1.65));
+list.push(new IMC("Oliver", 75, 1.70));
+list.push(new IMC("Marco", 80, 1.80));
+//Parcours de la liste
+list.forEach((element)=>{
+ element.display();
+});
+
+for (let i=0; i<list.length; i++) {
+list[i].display();
+}
+
+
 imc.display();
+
+
+//list.push(new IMC("Jade", 60, 1.65));
+console.log(list);
