3b6331ec3d2f9cd3d7196681970aecc234578e79
LauSalmon <salmon.laura@laposte.net>
2023-11-20 12:11:44 +0100

    ajout fichers exo19

---
+++ Exo19/main.js
@@ -0,0 +1,12 @@
+
+const titre = document.querySelector("h1");
+titre.style.fontSize = ("14px");
+titre.innerText = "Bienvenue sur QSMP";
+
+const bloc = document.querySelector("#bloc1");
+bloc.style.fontFamily = ("Arial");
+bloc.style.marginTop = ("30px");
+
+const paragraphe = document.querySelector("p");
+paragraphe.style.color = ("green");
+paragraphe.textContent = "Elle est actuellement sous la garde d' Antoine Daniel, Aypierre, Baghera Jones, Etoiles, Kameto et BadBoyHalo. "
