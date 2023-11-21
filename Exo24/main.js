81f03bcb17b3bad7c65198ea96f8227d83df40e2
LauSalmon <salmon.laura@laposte.net>
2023-11-21 16:39:39 +0100

    moif exo24/main.js

--- Exo24/main.js
+++ Exo24/main.js
@@ -1,8 +1,24 @@
-const ajouter = document.querySelector("#add");
-const tache = document.querySelector("#tasks");
+const add = document.querySelector("#add");
+const tasks = document.querySelector("#tasks");
+const del = document.querySelector("#delAllTask");
+const reload = document.querySelector("#reload");
 
 function addTask() {
-    ajouter.addEventListener("click", function(){
-        tache.createElement("p");
-    })
+    const task = document.querySelector("#task").value;
+   if (task != "") {
+        const paragraphe = document.createElement("p");
+        paragraphe.textContent = task;
+
+        tasks.appendChild(paragraphe);
+    }
+}
+
+function delAllTask () {
+    while(tasks.firstChild){
+        tasks.firstChild.remove()
+    }
+}
+
+function reload () {
+    windows.location.reload();
 }
