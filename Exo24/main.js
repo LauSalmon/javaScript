8a5930419f3bcb594f4ebc5761a5ea76be855bbb
LauSalmon <salmon.laura@laposte.net>
2023-11-21 15:56:47 +0100

    ajout fichiers

---
+++ Exo24/main.js
@@ -0,0 +1,8 @@
+const ajouter = document.querySelector("#add");
+const tache = document.querySelector("#tasks");
+
+function addTask() {
+    ajouter.addEventListener("click", function(){
+        tache.createElement("p");
+    })
+}
